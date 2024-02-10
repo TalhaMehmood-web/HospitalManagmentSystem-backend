26a62e13595c0aad5ad11ce44365e951c9f220b6
TalhaMehmood <talhamehmood991@gmail.com>
2024-02-11 01:43:51 +0500

    cloudinary and multer setup

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+import dotenv from "dotenv";
+dotenv.config();
+const cloudinaryConfig = {
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET
+}
+
+cloudinary.config(cloudinaryConfig);
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        });
+        fs.unlinkSync(localFilePath);
+        return response
+    } catch (error) {
+        fs.unlinkSync(localFilePath);
+        return null
+    }
+}
+export default uploadOnCloudinary;
